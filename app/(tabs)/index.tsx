1fed451a72a7fdaec138bd1040ef0862dee78be7
Leon Kwan <leonkwan46@gmail.com>
2025-05-31 13:27:44 +0100

    fix merge errors

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
-import { Image } from 'expo-image'
-import { StyleSheet, Text, View } from 'react-native'
 import ParallaxScrollView from '@/components/ParallaxScrollView'
+import { Image } from 'expo-image'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 
 export default function HomeScreen() {
   return (
@@ -12,41 +12,40 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+      <View style={styles.titleContainer}>
+        <Text>Welcome!</Text>
+      </View>
+      <View style={styles.stepContainer}>
+        <Text>Step 1: Try it</Text>
+        <Text>
+          Edit <Text>app/(tabs)/index.tsx</Text> to see changes.
           Press{' '}
-          <ThemedText type="defaultSemiBold">
+          <Text>
             {Platform.select({
               ios: 'cmd + d',
               android: 'cmd + m',
               web: 'F12',
             })}
-          </ThemedText>{' '}
+          </Text>{' '}
           to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+        </Text>
+      </View>
+      <View style={styles.stepContainer}>
+        <Text>Step 2: Explore</Text>
+        <Text>
+          {'Tap the Explore tab to learn more about what\'s included in this starter app.'}
+        </Text>
+      </View>
+      <View style={styles.stepContainer}>
+        <Text>Step 3: Get a fresh start</Text>
+        <Text>
+          {'When you\'re ready, run '}
+          <Text>npm run reset-project</Text> to get a fresh{' '}
+          <Text>app</Text> directory. This will move the current{' '}
+          <Text>app</Text> to{' '}
+          <Text>app-example</Text>.
+        </Text>
+      </View>
     </ParallaxScrollView>
   )
 }
--- components/Shared/Icons/IconSymbol.tsx
+++ components/Shared/Icons/IconSymbol.tsx
@@ -18,6 +18,8 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'gear': 'settings',
+  'bell.fill': 'notifications'
 } as IconMapping
 
 /**
